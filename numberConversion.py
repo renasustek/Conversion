fd84d144480e79472d0174a4ba73333fc3a7c5b5
renas <renasustek@gmail.com>
2018-08-20 22:43:31 +0100

    start up menu is completed for conversion

--- numberConversion.py
+++ numberConversion.py
@@ -1,1 +1,14 @@
 def startUpMenu():
+    conversionChoice1 = input("Enter your conversion choice one\n>>")
+    while conversionChoice1 != "hex" and conversionChoice1 != "denary" and conversionChoice1 != "binary":
+        conversionChoice1 = input("Invalid option pick hex,denary or binary\n>>")
+
+    conversionChoice2 = input("Enter your conversion choice two\n>>")
+    while conversionChoice2 != "hex" and conversionChoice2 != "denary" and conversionChoice2 != "binary":
+        conversionChoice2 = input("Invalid option pick hex,denary or binary\n>>")
+
+    finalConversionChoice = conversionChoice1+conversionChoice2
+
+    print(finalConversionChoice)
+
+startUpMenu()
